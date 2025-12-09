b00c9233d74d87fe717781e4d1ee5f8d9bb95a1c
36129382024.1M <laurasenac29@gmail.com>
2025-12-09 11:18:02 -0300

    ajuste em apk

---
+++ src/main/java/com/Aprova/demo/Controller/WebController.java
@@ -0,0 +1,20 @@
+package com.Aprova.demo.Controller;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+@CrossOrigin("*")
+public class WebController {
+
+    @GetMapping("/")
+    public String home() {
+        return "forward:/index.html";
+    }
+
+    @GetMapping("/install")
+    public String install() {
+        return "forward:/install.html";
+    }
+
+}
